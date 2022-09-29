feb7243a507e077e12044264c1af4cb373c2655e
shift <teixeira.matheus2626@gmail.com>
2022-09-29 17:12:03 -0300

    (portada/redsocial) - agregado efecto Parallax sobre los imágenes

--- src/Components/Portada/Portada.jsx
+++ src/Components/Portada/Portada.jsx
@@ -1,10 +1,22 @@
+import { useEffect, useRef } from 'react'
 import './portada.css'
 const Portada = ()=>{
+    console.log(window.screen.height, window.screen.width)
+    const dados = useRef();
+    useEffect(()=>{
+        window.addEventListener('scroll', ()=>{
+            // if(window.pageYOffset >=1000)alert('que onda capo')
+            // console.log(dados.current.style.backgroundPositionY)
+            dados.current.style.backgroundPositionY=-(window.pageYOffset*0.3)+"px";
+
+        })
+    }, [])
+    
     return(
         <section>
-            <div className='portada'>
-                <div className='flecha'></div>
-                <img className="" src="./assets/portada.jpeg" alt="portada" />
+            <div className='portada' ref={dados}>
+                <div className='flecha' style={{top:(window.screen.height - 300)+"px"}}></div>
+                <img className="image" src="./assets/imagen2.png" alt="portada"  />
             </div>
         </section>
     )
--- src/Components/RedSocial/RedSocial.jsx
+++ src/Components/RedSocial/RedSocial.jsx
@@ -1,11 +1,19 @@
 import './redSocial.css';
 import Button from "../Buttons/Button";
+import { useEffect, useRef } from 'react';
 
 const RedSocial=()=>{
-    
+    const redSocial = useRef()
+    useEffect(()=>{
+        window.addEventListener('scroll', ()=>{
+            let posicionScrollY = window.pageYOffset;
+            console.log(posicionScrollY);
+            redSocial.current.style.backgroundPositionY = -(posicionScrollY*0.3  )+"px"
+        })
+    },[])
     return(
         <section>
-            <div className="redSocialContainer">
+            <div className="redSocialContainer" ref={redSocial}>
             <img src="./assets/icono-instagram.svg" alt="" />
             <h4>#FLORCHUYMARIDO</h4>
             <p>"¡Preparate para nuestro gran día!"
